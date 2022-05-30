5c2c3f819697340161b0d815479f071da03065f7
Michał Wiliński <m.wilinski11@gmail.com>
2022-05-30 23:34:52 +0200

    some fixes

--- strcpy.asm
+++ strcpy.asm
@@ -24,9 +24,3 @@ section .text
         lea    rsi, [input]
         mov    rax, 0
         call   scanf wrt ..plt
-
-        ; copy input to output
-        mov     rsi, input
-        mov     rdi, output
-        mov     rcx, 64
-        call    memcpy
