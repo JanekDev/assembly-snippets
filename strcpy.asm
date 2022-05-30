3c499d75578204d30e765876cfdf34d56044b3ed
Michał Wiliński <m.wilinski11@gmail.com>
2022-05-30 23:34:33 +0200

    started strcpy

--- strcpy.asm
+++ strcpy.asm
@@ -3,21 +3,30 @@ default rel
 
 global  main
 
+; external functions
 extern  printf
+extern  scanf
 
 section .data
-    format      db 'Hello world!', 0xA, 0
+    formatter      db '%s'
+    newline        db '\n'
 
 section .bss
+    input          resb 64
+    output         resb 64
 
 section .text
     main:
         sub     rsp, 8
 
-        lea     rdi, [format]
-        mov     al, 0
-        call    printf wrt ..plt
+        ; get input
+        lea    rdi, [formatter]
+        lea    rsi, [input]
+        mov    rax, 0
+        call   scanf wrt ..plt
 
-        add     rsp, 8
-        sub     rax, rax
-        ret
+        ; copy input to output
+        mov     rsi, input
+        mov     rdi, output
+        mov     rcx, 64
+        call    memcpy
